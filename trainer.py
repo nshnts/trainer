e910c0c360b99b46e7785654e50ad84ba93dbfb0
nsatyarthy <6873481+nsatyarthy@users.noreply.github.com>
2019-03-20 16:22:44 +0900

    allow arbitrary number of training days

--- trainer.py
+++ trainer.py
@@ -3,83 +3,70 @@ import numpy as np
 
 
 class Color:
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
 
 
-def generate_program(lifts, workout_name):
-    # Assumes there are only two sections in the lifts:
-    #   -- Main Lifts
-    #   -- Accessories
+def create_weekly_program(lifts, n_days=4):
+
+    main, accessories = lifts
+    if n_days % 2 != 0:
+       raise ValueError('Cannot have odd-numbered workouts in a week')
+
+    days = [[] for _ in range(n_days)]
+
+    for i in range(int(n_days / 2)):
+       for lifts in main:
+          idx = np.random.randint(0, len(lifts))
+          days[i*2].append(lifts[idx])
+
+       for lifts in accessories:
+          idx = np.random.randint(0, len(lifts))
+          days[i*2 + 1].append(lifts[idx])
+
+       diff = len(days[i*2]) - len(days[i*2 + 1])
+       if diff > 0:
+           days[i*2 + 1].extend([ ' ' ] * diff)
+       elif -diff > 0:
+          days[i*2].extend([ ' ' ] * -diff)
+
     indent = ' ' * 10
-
-    print()
-    print(' ' * 5 + Color.GREEN + 'Workout ' + workout_name + Color.END)
-
-    program = {}
-    for section in lifts:
-        program[section['type']] = []
-
-    for section in lifts:
-        type = section['type']
-        for lift, excs in section['exercises'].items():
-            idx = np.random.randint(0, len(excs))
-            program[type].append(excs[idx])
-
-    main, accessories = program.values()
-    diff = len(main) - len(accessories)
-    if diff > 0:
-        accessories.extend([ ' ' ] * diff)
-    elif -diff > 0:
-        main.extend([ ' ' ] * -diff)
-
-    column_a, column_b = [section['type'] for section in lifts]
-
-    print()
-
-    print(indent, ('{:12}' + (' ' * 10)).format(Color.YELLOW + Color.UNDERLINE + column_a + Color.END),
-          ' ' * 9, ('{:12}' + (' ' * 10)).format(Color.YELLOW + Color.UNDERLINE + column_b + Color.END))
-    for x, y in zip(main, accessories):
-        s = indent + ' {:20} ' + (' ' * 10) + '{:20}'
-        print(s.format(x, y))
-    print()
-
-
-def weekly_program(lifts):
-    generate_program(lifts, 'A')
-    generate_program(lifts, 'B')
+    align = 30
+    s = '\n{}' + ''.join(['{:' + str(align) + '}'] * n_days)
+    days_str = [('Day '+str(i+1)) for i in range(n_days)]
+    print(Color.GREEN + s.format(indent, *days_str) + Color.END)
+    n_rows = len(days[0])
+    for row in range(n_rows):
+        line = indent
+        for day in days:
+            s = line + '{:<' + str(align) + '}'
+            line = s.format(day[row])
+        print(line)
+    print('\n')
 
 
 def main():
 
     lifts = [
-        # main lifts
-        {
-            'type': 'Main Lifts',
-            'exercises': {
-                'squat'    : [ 'Squat', 'Front Squat', 'Pause Squat', 'Pause Front Squat'   ],
-                'dl'       : [ 'Deadlift', 'Romanian Deadlift', 'Single-Leg Deadlift'       ],
-                'bench'    : [ 'Bench', 'DB Bench', 'CG Bench', 'Pause Bench'               ],
-                'back'     : [ 'T-Bar Row', 'DB Row', 'Pull-Up', 'Power Clean'              ],
-            }
-        },
-        #accessories
-        {
-            'type': 'Accessories',
-            'exercises': {
-                'shoulder' : [ 'Press', 'DB Press', 'Seated Press', 'Front Raise'           ],
-                'bi'       : [ 'EZ-Bar Curl', 'DB Curl', 'BB Curl', 'Hammer Curl'           ],
-                'tri'      : [ 'Dip', 'LTE', 'DB LTE', 'Ring Push-Up'                       ],
-                'abs'      : [ 'Torque', 'Hanging Leg Raise', 'Sit-Up', 'Plank'             ]
-            }
-        }
+        [ # main lifts
+           [ 'Squat', 'Front Squat', 'Pause Squat', 'Pause Front Squat'   ], # squat
+           [ 'Deadlift', 'Romanian Deadlift', 'Single-Leg Deadlift'       ], # deadlift
+           [ 'Bench', 'DB Bench', 'CG Bench', 'Pause Bench'               ], # bench
+           [ 'T-Bar Row', 'DB Row', 'Pull-Up', 'Power Clean'              ]  # pull
+        ],
+        [ # accessories
+            [ 'Press', 'DB Press', 'Seated Press', 'Front Raise'           ], # shoulder
+            [ 'EZ-Bar Curl', 'DB Curl', 'BB Curl', 'Hammer Curl'           ], # bi
+            [ 'Dip', 'LTE', 'DB LTE', 'Ring Push-Up'                       ], # tri
+            [ 'Torque', 'Hanging Leg Raise', 'Sit-Up', 'Plank'             ]  # abs
+        ]
     ]
 
-    weekly_program(lifts)
+    create_weekly_program(lifts)
 
 
 if __name__ == '__main__':
